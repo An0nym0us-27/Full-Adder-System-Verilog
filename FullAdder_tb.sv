9e4f4389d12647f85f8142d4e298dd9bc82a671a
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-01-10 22:08:52 -0800

    Update FullAdder_tb.sv

--- FullAdder_tb.sv
+++ FullAdder_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 10ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/13/2022 08:21:49 PM
-// Design Name: 
-// Module Name: FullAdder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module FullAdder_tb;
     logic a, b, cin;
